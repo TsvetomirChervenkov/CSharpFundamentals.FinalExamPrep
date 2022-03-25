3edd04d24863bed98666011894501ef33a2de071
Ceco ceco <tsvetomirpch78@gmail.com>
2022-03-25 19:26:28 +0200

    Add project files.

---
+++ T01.The Imitation Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T01.The_Imitation_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
