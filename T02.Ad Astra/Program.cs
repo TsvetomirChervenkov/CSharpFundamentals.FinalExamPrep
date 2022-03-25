88ca5165af857a08cc2a982ceaf7a221081f0664
Ceco ceco <tsvetomirpch78@gmail.com>
2022-03-25 20:11:02 +0200

    Added a task

---
+++ T02.Ad Astra/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T02.Ad_Astra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = Console.ReadLine();
+            string validInput = @"([#|])(?<product>[A-Z-a-z\s]+)\1(?<date>[0-9]{2,}/[0-9]{2,}/[0-9]{2,})\1(?<calories>\d+)\1";
+            
+            MatchCollection matches = Regex.Matches(code, validInput);
+            int totalCalories = 0;
+            foreach (Match match in matches)
+            {
+                totalCalories += int.Parse(match.Groups["calories"].Value);
+            }
+            int totalDays = totalCalories / 2000;
+            Console.WriteLine($"You have food to last you for: {totalDays} days!");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Item: {match.Groups["product"].Value}, Best before: {match.Groups["date"].Value}, Nutrition: {match.Groups["calories"].Value}");
+            }
+        }
+    }
+}
