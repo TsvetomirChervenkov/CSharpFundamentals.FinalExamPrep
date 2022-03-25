e9fa1be4cdc14ff499f02cf4c44b25c9f13f43f7
Ceco ceco <tsvetomirpch78@gmail.com>
2022-03-25 19:46:45 +0200

    Added a problem

--- T01.The Imitation Game/Program.cs
+++ T01.The Imitation Game/Program.cs
@@ -6,7 +6,33 @@ namespace T01.The_Imitation_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string encryptedMessage = Console.ReadLine();
+            string code = string.Empty;
+            while ((code= Console.ReadLine()) != "Decode")
+            {
+                string[] commandArgs = code.Split('|');
+                string command = commandArgs[0];
+                if (command == "Move")
+                {
+                    int letters = int.Parse(commandArgs[1]);
+                    string substring = encryptedMessage.Substring(0, letters);
+                    encryptedMessage = encryptedMessage.Remove(0, letters);
+                    encryptedMessage = encryptedMessage + substring;
+                }
+                else if (command == "Insert")
+                {
+                    int index = int.Parse(commandArgs[1]);
+                    string substring = commandArgs[2];
+                    encryptedMessage = encryptedMessage.Insert(index, substring);
+                }
+                else if (command == "ChangeAll")
+                {
+                    string substring = commandArgs[1];
+                    string replacement = commandArgs[2];
+                    encryptedMessage = encryptedMessage.Replace(substring, replacement);
+                }
+            }
+            Console.WriteLine($"The decrypted message is: " + encryptedMessage);
         }
     }
 }
