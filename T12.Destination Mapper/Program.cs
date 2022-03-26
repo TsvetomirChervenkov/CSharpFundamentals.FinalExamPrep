44c8e7b9045e98807d44fd7372d1f10c8f8cd8ef
Ceco ceco <tsvetomirpch78@gmail.com>
2022-03-26 10:54:28 +0200

    added this new task, using regex

---
+++ T12.Destination Mapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T12.Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string validator = @"([=\/])(?<destination>[A-Z][A-Za-z]{2,})+\1";
+
+            MatchCollection matches = Regex.Matches(input, validator);
+
+            int lenght = 0;
+            int counter = 0;
+            string[] destinations = new string[matches.Count];
+
+            foreach (Match match in matches)
+            {
+                lenght+= match.Groups["destination"].Value.Length;
+                destinations[counter] = match.Groups["destination"].Value;
+                counter++;
+
+            }
+            Console.WriteLine("Destinations: " + string.Join(", ", destinations));
+            Console.WriteLine($"Travel Points: {lenght}");
+        }
+    }
+}
