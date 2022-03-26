3d7fd7f03ccc9cd2c8f8badf10b746e757453556
Ceco ceco <tsvetomirpch78@gmail.com>
2022-03-26 18:02:45 +0200

    Added another task

---
+++ T22.Mirror Words/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T22.Mirror_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string validator = @"([@#])(?<pair>[A-Za-z]{3,})\1\1(?<pair2>[A-Za-z]{3,})\1";
+            MatchCollection matches = Regex.Matches(word, validator);
+
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+
+            }
+            else
+            {
+                Console.WriteLine($"{matches.Count} word pairs found!");
+
+            }
+            string newstring = string.Empty;
+
+            foreach (Match match in matches)
+            {
+                char[] reversed = match.Groups["pair2"].Value.ToCharArray();
+                Array.Reverse(reversed);
+                string newReversed = string.Join("", reversed);
+                if (match.Groups["pair"].Value == newReversed)
+                {
+                    string oneMatch = $"{match.Groups["pair"].Value} <=> {match.Groups["pair2"].Value}, ";
+                    newstring += string.Join(", ", oneMatch);
+                }
+            }
+            if (newstring == string.Empty)
+            {
+                Console.WriteLine("No mirror words!");
+                return;
+            }
+            newstring = newstring.TrimEnd(' ', ',');
+            Console.WriteLine("The mirror words are:");
+            Console.WriteLine(newstring);
+
+        }
+    }
+}
